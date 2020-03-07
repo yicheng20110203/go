7cbdebe15ad3e770e17ba6af204a14a271ec2242
dingyong <dingyong@qutoutiao.net>
2020-03-08 00:11:09 +0800

    add middleware comments

--- app/LogMiddleware.go
+++ app/LogMiddleware.go
@@ -2,9 +2,11 @@ package app
 
 import (
 	"fmt"
+
 	"github.com/gin-gonic/gin"
 )
 
+// LoggerMiddleware easy middleware
 func LoggerMiddleware() gin.HandlerFunc {
 	fmt.Println("Hello world!")
 	return func(context *gin.Context) {
